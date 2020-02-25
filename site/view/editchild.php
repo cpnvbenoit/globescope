50416d018ab5e035e86d01758a4765c6fa040e56
SimonCuany <47849679+SimonCuany@users.noreply.github.com>
2020-02-25 16:34:07 +0100

    Update input to be more pretty

--- site/view/editchild.php
+++ site/view/editchild.php
@@ -11,7 +11,7 @@ ob_start();
 <h1 align="center">Modifier les infos</h1>
 
 
-<table class="table" border="1"  align="center">
+<table class="table" border="1" align="center">
     <thead>
     <th>
         Latitude / Longitude / Meridien
@@ -38,71 +38,86 @@ ob_start();
         Modifier
     </th>
     </thead  align="center">
-    <tbody  align="center">
+    <tbody align="center">
 
-        <?php
-         foreach ($childs as $child) {
-             if ($child['IDImage'] == $idchild) {
+    <?php
+    foreach ($childs as $child) {
+        if ($child['IDImage'] == $idchild) {
 
-                 ?>
-                 <tr>
-                     <td>
-                         <p><?= $child['lat'] ?> / <?= $child['lon'] ?> / <?= $child['mer'] ?></p>
-                     </td>
-                     <td>
-                         <p><?= $child['IDPlace'] ?></p>
-                     </td>
-                     <td>
-                         <p><?= $child['IDImage'] ?></p>
-                     </td>
-                     <td>
-                         <p><?= $child['Pseudo'] ?></p>
-                     </td>
-                     <td>
-                         <p><?= $child['Droit'] ?></p>
-                     </td>
-                     <td>
-                         <p>
-                             <?= $child['Slogan'] ?>
-                         </p>
-                     </td>
-                     <td>
-                         <p><?= $child['Provenance'] ?></p>
-                     </td>
+            ?>
+            <tr>
+                <td>
+                    <p><?= $child['lat'] ?> / <?= $child['lon'] ?> / <?= $child['mer'] ?></p>
+                </td>
+                <td>
+                    <p><?= $child['IDPlace'] ?></p>
+                </td>
+                <td>
+                    <p><?= $child['IDImage'] ?></p>
+                </td>
+                <td>
+                    <p><?= $child['Pseudo'] ?></p>
+                </td>
+                <td>
+                    <p><?= $child['Droit'] ?></p>
+                </td>
+                <td>
+                    <p>
+                        <?= $child['Slogan'] ?>
+                    </p>
+                </td>
+                <td>
+                    <p><?= $child['Provenance'] ?></p>
+                </td>
 
-                 </tr>
-                <!-- le temp de benoit va dans TEMPBENOIT.TXT-->
+            </tr>
+            <!-- le temp de benoit va dans TEMPBENOIT.TXT-->
 
-                 <?php
-             }
-         }
-      ?>
+            <?php
+        }
+    }
+    ?>
 
     </tbody>
 </table>
-<table>
+<br>
+<table align="center">
     <tr>
         <td>
-            <input type="text" disabled id="meridien" >
-            <input type="text" disabled id="latitude">
-            <input type="text" disabled id="longitude">
-            <input type="text" disabled id="idplace">
-            <input type="text" disabled id="idimage">
-            <input type="text" id="Pseudo">
-            <input type="text" id="Droit">
-            <input type="text" id="Slogan">
+
+            <input type="text" disabled class="edit-disabled" id="latitude">
+
+
+        </td>
+        <td>
+            <input type="text" disabled class="edit-disabled" id="meridien">
+        </td>
+        <td>
+            <input type="text" disabled class="edit-disabled" id="longitude">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="text" disabled class="edit-disabled" id="idplace">
+        </td>
+        <td>
+            <input type="text" disabled class="edit-disabled" id="idimage">
+        </td>
+        <td>
             <input type="text" id="team">
-
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="text" id="Droit">
         </td>
         <td>
-
+            <input type="text" id="Slogan">
         </td>
         <td>
-
+            <input type="text" id="Pseudo">
         </td>
-        <td>
 
-        </td>
     </tr>
 
 </table>
