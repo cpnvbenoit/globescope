d6f4a62d2aab03161cb1482a460802ccc4802aa6
SimonCuany <simon.cuany@cpnv.ch>
2020-04-20 16:44:32 +0200

    secret

---
+++ site/view/testsecret.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: simon.cuany
+ * Date: 13.02.2020
+ * Time: 11.19
+ */
+?>
+<head>
+    <title>Its a secret</title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://image.flaticon.com/icons/svg/2720/2720077.svg" />
+</head>
+<body style="background-color: #4CAF50"><br>
+<h1 align="center" style="color: white">Bienvenue - Welcome - Willkommen - benvenuto !</h1>
+<div align="center">
+    <br><br><br>
+    <a href="https://www.youtube.com/watch?v=BBGEG21CGo0" >
+<img src="https://upload.wikimedia.org/wikipedia/commons/5/54/Bg-easter-eggs.jpg" width="25%" >
+    </a>
+</div>
+<div align="center" style="color: white">
+<h2>FR : Cette page est un secret et je vous félicite de l'avoir trouvée !</h2>
+<h2>ANG : This page is a secret and I congratulate you for finding it!</h2>
+<h2>DEU : Diese Seite ist ein Geheimnis und ich gratuliere Ihnen, dass Sie es gefunden haben!</h2>
+<h2>ITA : Questa pagina è un segreto e mi congratulo con te per averlo trovato!</h2>
+</div>
+
+<h1 align="center" style="color: white">Signé : Vos deux développeurs qui vous aiment : <br> Benoit Pierrehumbert et Simon Cuany du CPNV de Ste-Croix ! </h1>
+
+</body>
+
