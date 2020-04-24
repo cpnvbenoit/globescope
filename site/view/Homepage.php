7a1bcfffc249234bdec742634fe0b8576bdb3f2b
SimonCuany <simon.cuany@cpnv.ch>
2020-04-24 12:05:12 +0200

    update design in homepage

--- site/view/Homepage.php
+++ site/view/Homepage.php
@@ -15,35 +15,45 @@
     <title>Accueil</title>
 </head>
 <body class="body">
-<img src="https://edm.mycpnv.ch/boutique/Logo_bleu_RGB.png " class="imgup">
 
+
+<a href="#">
+<img src="https://cdn.icon-icons.com/icons2/665/PNG/512/earth_icon-icons.com_60279.png" alt="" class="imglobe" align="right">
+</a>
+
+<div>
+    <label for="logoedm" class="labelup"><h3>Avec le Globe «Un Monde Plus Juste», les enfants défendent leurs
+            droits</h3></label>
+    <a href="https://www.edm.ch/fr/"><img src="https://edm.mycpnv.ch/images/Logo_bleu_RGB.png" class="imgup"
+                                          id="logoedm"> </a>
+</div>
 <ul>
-    <li><a  href="https://www.edm.ch/fr/">Home</a></li>
-    <li><a href="https://edm.mycpnv.ch/">Globe</a></li>
+    <li><a href="https://www.edm.ch/fr/">Home</a></li>
+    <li><a href="https://edm.mycpnv.ch/">Globe - EDM</a></li>
     <li><a href="https://edm.mycpnv.ch/boutique/">Boutique</a></li>
     <li><a href="https://edm.mycpnv.ch/forum/">Forum</a></li>
 </ul>
-    <h1 align="center">Bienvenue sur la page d'accueil d'enfants du monde !</h1>
-    <table align="center">
-        <td>
-            <!-- Acccès globe https://edm.mycpnv.ch/boutique/Logo_bleu_RGB.png  -->
-            <a href="https://edm.mycpnv.ch/"><img
-                        src="https://www.edm.ch/images/stories/articles/sensibilisation/UMJ/globevirtuel.jpg"
-                        class="imghome1" id="globe"></a>
-        </td>
-        <td class="imghome2">
-            <!-- Acccès Boutique -->
-           <a href="https://edm.mycpnv.ch/boutique"><img
-                        src="https://clipartart.com/images/shopping-cart-icon-clipart-1.jpg" id="globe" class="imgshop"></a>
-        </td>
-        <td class="imghome3">
-            <!-- Acccès Forum -->
-            <a href="https://edm.mycpnv.ch/forum">
-                <img src="https://image.flaticon.com/icons/svg/81/81119.svg" id="globe"
-                     class="imgforum">
-            </a>
-        </td>
-    </table>
+<h1 align="center">Bienvenue sur la page d'accueil d'enfants du monde !</h1>
+<table align="center">
+    <td>
+        <!-- Acccès globe   -->
+        <a href="?action=home"><img
+                    src="https://www.edm.ch/images/stories/articles/sensibilisation/UMJ/globevirtuel.jpg"
+                    class="imghome1" id="globe"></a>
+    </td>
+    <td class="imghome2">
+        <!-- Acccès Boutique -->
+        <a href="?action=boutique"><img
+                    src="https://clipartart.com/images/shopping-cart-icon-clipart-1.jpg" id="globe" class="imgshop"></a>
+    </td>
+    <td class="imghome3">
+        <!-- Acccès Forum -->
+        <a href="?action=forum">
+            <img src="https://image.flaticon.com/icons/svg/81/81119.svg" id="globe"
+                 class="imgforum">
+        </a>
+    </td>
+</table>
 
 </body>
 </html>
