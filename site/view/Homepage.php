eb842fa9148f9bd5cd2f1716ee31edccd3c9ed3a
SimonCuany <simon.cuany@cpnv.ch>
2020-04-24 13:16:45 +0200

    Add footer WIP some bugs with display

--- site/view/Homepage.php
+++ site/view/Homepage.php
@@ -12,13 +12,15 @@
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="test.css">
     <title>Accueil</title>
 </head>
 <body class="body">
 
 
 <a href="#">
-<img src="https://cdn.icon-icons.com/icons2/665/PNG/512/earth_icon-icons.com_60279.png" alt="" class="imglobe" align="right">
+    <img src="https://cdn.icon-icons.com/icons2/665/PNG/512/earth_icon-icons.com_60279.png" alt="" class="imglobe"
+         align="right">
 </a>
 
 <div>
@@ -34,7 +36,7 @@
     <li><a href="https://edm.mycpnv.ch/forum/">Forum</a></li>
 </ul>
 <h1 align="center">Bienvenue sur la page d'accueil d'enfants du monde !</h1>
-<table align="center">
+<table align="center" class="imgchoix">
     <td>
         <!-- Acccès globe   -->
         <a href="?action=home"><img
@@ -54,6 +56,136 @@
         </a>
     </td>
 </table>
+<br>
+<!-- Code du Footer de EDM -->
+<div id=footer-wrapper>
+    <div class="container-fluid inner ">
+        <section id=footer-modules class=clearfix>
+            <div class="inner clearfix n4">
+                <div id=footer-module-1 class="flexiblemodule ">
+                    <div class="inner clearfix">
+                        <div class=moduletable_menu>
+                            <ul class="nav menu mod-list">
+                                <li class="item-2640 parent"><a href=/fr/notre-mission>Mission</a></li>
+                                <li class="item-3691 parent"><a href=/fr/education>Education</a></li>
+                                <li class="item-3692 parent"><a href=/fr/sante>Santé</a></li>
+                                <li class="item-4037 parent"><a href=/fr/sensibilisation>Sensibilisation</a></li>
+                                <li class="item-2617 parent"><a href=/fr/aider>Nous soutenir</a></li>
+                                <li class="item-2644 parent"><a href=/fr/media-publication>Média &amp; publications</a>
+                                </li>
+                                <li class="item-2652 parent"><a href=/fr/contact-fr>Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id=footer-module-2 class="flexiblemodule ">
+                    <div class="inner clearfix">
+                        <div class=moduletable>
+                            <div class=custom><p><strong>Compte bancaire</strong></p>
+                                <p>Credit Suisse<br/>IBAN: CH 19 0483 5039 5693 8100 0</p>
+                                <p><strong>Compte postal</strong></p>
+                                <p>CCP 12-415-4</p>
+                                <p>&nbsp;</p>
+                                <p><strong>Numéro IDE: </strong>CHE-107.976.084</p>
+                                <p>Votre don est déductible fiscalement. Vous recevrez chaque année, fin février, une
+                                    attestation pour vos dons de l’année précédente, que vous pourrez joindre à votre
+                                    déclaration fiscale.</p>
+                                <p><strong><a title="Conditions générales d'Enfants du Monde"
+                                              href=/components/com_fp_donation/conditions-fr.html>Conditions
+                                            générales</a> | <a title="Protection des données d'Enfants du Monde"
+                                                               href=/fr/protection-donnees/>Protection des
+                                            données</a></strong>
+                                </p></div>
+                        </div>
+                    </div>
+                </div>
+                <div id=footer-module-3 class="flexiblemodule ">
+                    <div class="inner clearfix">
+                        <div class=moduletable>
+                            <div class=custom><p><a href=/fr/media-publication/actualites/derniere-newsletter/newsletter
+                                                    style="background-color: transparent;"><img
+                                                src=/images/stories/icons/newsletter_white.png alt="newsletter white"
+                                                width=40
+                                                height=30 style="vertical-align: middle; margin-right: 10px;"/>S'abonner</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class=moduletable>
+                            <div class=custom><p>Suivez-nous!</p>
+                                <p><a href=https://www.facebook.com/EnfantsDuMonde/ target=_blank
+                                      rel="noopener noreferrer"><img style="margin-right: 10px;"
+                                                                     src=/images/stories/icons/fa-facebook.png
+                                                                     alt="fa facebook" width=40 height=40/></a><a
+                                            href=https://www.youtube.com/user/edmsuisse target=_blank
+                                            rel="noopener noreferrer"><img style="margin-right: 10px;"
+                                                                           src=/images/stories/icons/fa-youtube.png
+                                                                           alt="fa youtube" width=40 height=40/></a><a
+                                            href=https://www.flickr.com/photos/enfants_du_monde/ target=_blank
+                                            rel="noopener noreferrer"><img style="margin-right: 10px;"
+                                                                           src=/images/stories/icons/fa-flickr.png
+                                                                           alt="fa flickr" width=40 height=40/></a><a
+                                            href=https://www.instagram.com/enfantsdumonde.ch/ target=_blank
+                                            rel="noopener noreferrer"><img style="margin-right: 10px;"
+                                                                           src=/images/stories/icons/fa-instagram.png
+                                                                           alt="fa instagram" width=40 height=40/></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id=footer-module-4 class="flexiblemodule ">
+                    <div class="inner clearfix">
+                        <div class=moduletable>
+                            <div class=custom><p><a href=/fr/aider/confiance/zewo-controles target=_blank
+                                                    rel="noopener noreferrer"><img
+                                                style="margin-right: 10px; margin-left: 10px; float: right;"
+                                                src=/images/stories/icons/items/zewo.png alt=zewo width=73
+                                                height=79/></a></p>
+                                <p>&nbsp;</p>
+                                <p>&nbsp;</p>
+                                <p>&nbsp;</p>
+                                <p>&nbsp;</p>
+                                <p><a title="Label employeur 1+ pour tous"
+                                      href=https://www.ge.ch/dossier/1-tous-label-faveur-emploi target=_blank
+                                      rel="noopener noreferrer"><img
+                                                style="margin-right: 10px; margin-left: 10px; float: right;"
+                                                src=/images/stories/icons/items/unpluspourtous.png alt=zewo width=98
+                                                height=74/></a>
+                                </p></div>
+                        </div>
+                    </div>
+                </div>
+                <div class=clr></div>
+            </div>
+        </section>
+        <div id=footer-don>
+            <div class="inner clearfix">
+                <div class=moduletable>
+                    <div class=custom><p><a href=/fr/aider/don class=button title="Faire un don à Enfants du Monde"><img
+                                        src=/images/stories/icons/don-grey.png alt="don grey" width=24 height=26
+                                        style="vertical-align: middle; margin-right: 5px;"/>Faire un don</a></p></div>
+                </div>
+            </div>
+        </div>
+        <div id=footer-copyright>
+            <div class="inner clearfix">
+                <div class=moduletable>
+                    <div class=custom><p><strong>Enfants du Monde</strong> | Route de Ferney 150 | CP 2100&nbsp;| 1211
+                            Genève 2 | Tél : +41 22 798 88 81 | <a
+                                    href="javascript:/* This email address is being protected from spambots.*/">info[at]edm.ch<span
+                                        class="cloaked_email ep_96cfdbb6" style=display:none;><span
+                                            data-ep-ab883=&#105;&#110;
+                                            data-ep-bf95e=h><span
+                                                data-ep-ab883=fo data-ep-bf95e=.c><span data-ep-bf95e=dm
+                                                                                        data-ep-ab883=&#64;&#101;></span></span></span></span></a><br/>2016&nbsp;©
+                            Enfants du Monde | Gestion du site par App'n Web | Conception &amp; Design par 8bitstudio
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
